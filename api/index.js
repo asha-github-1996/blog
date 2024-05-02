e587cf5b90189a94b4c3b2211ce5948f01825cd9
asha <ashapoulose0@gmail.com>
2024-05-02 13:02:36 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+//Database connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Connected to db");
+  },
+  (err) => {
+    console.log("Something went wrong" + err);
+  }
+);
 
 const app = express();
 
